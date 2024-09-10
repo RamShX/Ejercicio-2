479927469feedb03e23bfc0ba1c3d3a135840155
Ramshley Polanco V. <ramshleyv@gmail.com>
2024-09-10 12:02:46 -0400

    completado

--- obj/Debug/net8.0/P2 ejercicio 2.AssemblyInfo.cs
+++ obj/Debug/net8.0/P2 ejercicio 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P2 ejercicio 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83309deae916fd849d7128d4fd41e2fcab59a539")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2 ejercicio 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P2 ejercicio 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
